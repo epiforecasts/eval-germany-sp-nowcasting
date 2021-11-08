25110b55c528505c336d279bdf865c8c474efc0b
seabbs <s.e.abbott12@gmail.com>
2021-11-08 22:19:09 +0000

    add models

--- R/models.R
+++ R/models.R
@@ -1,5 +1,193 @@
+summarise_nowcast <- function(nowcast, model,
+                              probs = c(
+                                0.025, 0.05, seq(0.1, 0.9, by = 0.1),
+                                0.95, 0.975
+                              )) {
+  daily <- summary(nowcast, type = "nowcast", probs = probs)
+
+  samples <- summary(nowcast, type = "nowcast_samples")
+
+  cols <- c("confirm", "sample")
+  samples[, (cols) := lapply(.SD, data.table::frollsum, n = 7),
+    .SDcols = cols, by = ".draw"
+  ][!is.na(sample)]
+
+  # Summarise 7 day nowcast
+  seven_day <- enw_summarise_samples(samples, probs = probs)
+
+  out <- data.table::data.table(
+    model = model,
+    nowcast_date = max(daily$reference_date),
+    daily = list(daily),
+    seven_day = list(samples)
+  )
+  return(out[])
+}
+
+fixed_epinowcast <- function(obs, max_delay = 40, ...) {
+  pobs <- enw_preprocess_data(
+    obs,
+    max_delay = max_delay, group = "age_group"
+  )
+
+  reference_effects <- enw_formula(pobs$metareference)
+  report_effects <- enw_formula(pobs$metareport)
+
+  nowcast <- epinowcast(
+    pobs,
+    reference_effects = reference_effects,
+    report_effects = report_effects,
+    ...
+  )
+
+  out <- summarise_nowcast(nowcast, model = "Reference: Fixed, Report: Fixed")
+  return(out[])
+}
+
+dow_epinowcast <- function(obs, max_delay = 40, ...) {
+  pobs <- enw_preprocess_data(
+    obs,
+    max_delay = max_delay, group = "age_group"
+  )
+
+  reference_effects <- enw_formula(pobs$metareference)
+  report_effects <- enw_formula(pobs$metareport, random = "day_of_week")
+
+  nowcast <- epinowcast(
+    pobs,
+    reference_effects = reference_effects,
+    report_effects = report_effects,
+    ...
+  )
+
+  out <- summarise_nowcast(
+    nowcast,
+    model = "Reference: Fixed, Report: Day of week"
+  )
+  return(out[])
+}
+
+age_epinowcast <- function(obs, max_delay = 40, ...) {
+  pobs <- enw_preprocess_data(
+    obs,
+    max_delay = max_delay, group = "age_group"
+  )
+
+  reference_effects <- enw_formula(pobs$metareference, random = "age_group")
+  report_effects <- enw_formula(pobs$metareport, random = "day_of_week")
+
+  nowcast <- epinowcast(
+    pobs,
+    reference_effects = reference_effects,
+    report_effects = report_effects,
+    ...
+  )
+
+  out <- summarise_nowcast(
+    nowcast,
+    model = "Reference: Age, Report: Day of week"
+  )
+  return(out[])
+}
+
+week_epinowcast <- function(obs, max_delay = 40, ...) {
+  pobs <- enw_preprocess_data(
+    obs,
+    max_delay = max_delay, group = "age_group"
+  )
+
+  metareference <- enw_add_cumulative_membership(
+    pobs$metareference[[1]],
+    feature = "week"
+  )
+  reference_effects <- enw_formula(
+    metareference,
+    random = "age_group", custom_random = "cweek"
+  )
+  report_effects <- enw_formula(pobs$metareport, random = "day_of_week")
+
+  nowcast <- epinowcast(
+    pobs,
+    reference_effects = reference_effects,
+    report_effects = report_effects,
+    ...
+  )
+
+  out <- summarise_nowcast(
+    nowcast,
+    model = "Reference: Age and week, Report: Day of week"
+  )
+  return(out[])
+}
+
+age_week_epinowcast <- function(obs, max_delay = 40, ...) {
+  pobs <- enw_preprocess_data(
+    obs,
+    max_delay = max_delay, group = "age_group"
+  )
+
+  metareference <- pobs$metareference[[1]]
+
+  fixed_form <- as.formula(paste0(
+    "~ 1 + age_group + ",
+    paste(paste0(
+      "age_group:",
+      grep("cweek", colnames(metareference), value = TRUE),
+      collapse = " + "
+    )),
+    " + ",
+    paste(grep("cweek", colnames(metareference), value = TRUE),
+      collapse = " + "
+    )
+  ))
+  fixed <- enw_design(fixed_form, metareference,
+    no_contrasts = TRUE,
+    sparse = TRUE
+  )
+
+  effects <- enw_effects_metadata(fixed$design)
+
+  effects <- enw_add_pooling_effect(effects, "cweek", "cweek")
+  effects <- enw_add_pooling_effect(effects, "age_group", "age_group")
+  for (i in unique(metareference$age_group)) {
+    effects <- enw_add_pooling_effect(
+      effects, c("cweek", paste0("age_group", i)), paste0(i, "_walk"),
+      finder_fn = function(effect, pattern) {
+        grepl(pattern[1], effect) & startsWith(effect, pattern[2])
+      }
+    )
+  }
+  effects[grepl(":", effects), age_group := 0]
+
+  form <- as.formula(
+    paste0(
+      "~ 0 + fixed + cweek + age_group + ",
+      paste(paste0("`", unique(metareference$age_group), "_walk`"),
+        collapse = " + "
+      )
+    )
+  )
+  random <- enw_design(form, effects, sparse = FALSE)
+  reference_effects <- list(fixed = fixed, random = random)
+
+  report_effects <- enw_formula(pobs$metareport, random = "day_of_week")
+
+  nowcast <- epinowcast(
+    pobs,
+    reference_effects = reference_effects,
+    report_effects = report_effects,
+    ...
+  )
+
+  out <- summarise_nowcast(
+    nowcast,
+    model = "Reference: Age and week by age, Report: Day of week"
+  )
+  return(out[])
+}
+
 independent_epinowcast <- function(obs, max_delay = 40, ...) {
-  pobs_ind <- enw_preprocess_data(obs, max_delay = max_delay)
+  pobs <- enw_preprocess_data(obs, max_delay = max_delay)
 
   metareference <- enw_add_cumulative_membership(
     pobs_ind$metareference[[1]],
@@ -10,25 +198,15 @@ independent_epinowcast <- function(obs, max_delay = 40, ...) {
   report_effects <- enw_formula(pobs_ind$metareport, random = "day_of_week")
 
   nowcast <- epinowcast(
-    pobs_ind,
+    pobs,
     reference_effects = reference_effects,
     report_effects = report_effects,
     ...
   )
 
-  summary <- summary(
+  out <- summarise_nowcast(
     nowcast,
-    type = "nowcast",
-    probs = c(0.025, 0.05, seq(0.1, 0.9, by = 0.1), 0.95, 0.975)
+    model = "Independent by age, Reference: Week, Report: Day of week"
   )
-
-  samples <- summary(nowcast, type = "nowcast_samples")
-
-  out <- data.table::data.table(
-    model = "Independent by age, Reference: Week, Report: Day of week",
-    nowcast_date = max(summary$reference_date),
-    summary = list(summary),
-    samples = list(samples)
-  )
-  return(out)
+  return(out[])
 }
