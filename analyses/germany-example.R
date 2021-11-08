b86a20b361f33f80d1dab9333da4da8483c61214
seabbs <s.e.abbott12@gmail.com>
2021-11-08 13:05:57 +0000

    update data ingestion

--- analyses/germany-example.R
+++ analyses/germany-example.R
@@ -12,26 +12,7 @@ walk(functions, source)
 options(mc.cores = 4)
 
 # get processed germany data and reprocess
-germany_hosp <- fread(
-  "https://raw.githubusercontent.com/jbracher/hospitalization-nowcast-hub/main/data-truth/COVID-19/COVID-19_hospitalizations_preprocessed.csv" # nolint
-)
-germany_hosp <- melt(
-  germany_hosp,
-  variable.name = "delay",
-  value.name = "confirm",
-  id.vars = c("date", "location", "age_group")
-)
-setnames(germany_hosp, "date", "reference_date")
-
-germany_hosp[, report_date := as.Date(reference_date) + 0:(.N - 1),
-  by = c("reference_date", "location", "age_group")
-]
-germany_hosp <- germany_hosp[report_date <= max(reference_date)]
-germany_hosp[, delay := NULL]
-germany_hosp[is.na(confirm), confirm := 0]
-germany_hosp[, confirm := cumsum(confirm),
-  by = c("reference_date", "location", "age_group")
-]
+germany_hosp <- get_germany_hospitalisations()
 
 # national only and only for the last 28 days of data
 germany_nat_hosp <- germany_hosp[location == "DE"]
