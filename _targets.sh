772c6af7ecc4f06520221d25fa0d007cdad7d43f
Sam Abbott <contact@samabbott.co.uk>
2021-11-17 22:13:01 +0000

    ready to run all nowcasts from september

--- _targets.sh
+++ _targets.sh
@@ -4,5 +4,5 @@
 Rscript -e "rmarkdown::render('_targets.Rmd')" 
 
 nohup \
-  Rscript -e "targets::tar_make_future(workers = floor(future::availableCores() / 4))" \
+  Rscript -e "targets::tar_make_future(workers = floor(future::availableCores() / 2))" \
   > targets.log 2>&1 &
