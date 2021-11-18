c7c4b65e49570fe53fdbd7667aff8c779211f542
Sam Abbott <contact@samabbott.co.uk>
2021-11-18 18:33:26 +0000

    fix knitr chunk info

--- _targets_r/targets/scored_nowcasts, tar_simple.R
+++
@@ -1,4 +0,0 @@
-summarised_nowcast[reference_date >= (report_date - 7)][
-                   reference_date < (report_date - 28)][,
-                   holiday := NULL][,
-                   horizon := as.numeric(reference_date - report_date)]
