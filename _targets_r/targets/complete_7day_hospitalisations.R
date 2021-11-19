5e8da504d547a1a0d731d901e2e0504285623d55
Sam Abbott <contact@samabbott.co.uk>
2021-11-19 00:37:30 +0000

    handle complete data correctly

--- _targets_r/targets/complete_7day_hospitalisations.R
+++ _targets_r/targets/complete_7day_hospitalisations.R
@@ -1,5 +1,6 @@
 tar_target(complete_7day_hospitalisations, {
-  latest_7day_hospitalisations[reference_date < (report_date - 28)][,
-                               horizon := as.numeric(reference_date - report_date)
-                              ]
+  latest_7day_hospitalisations[
+    reference_date < (max(reference_date) - 28)][,
+    horizon := as.numeric(reference_date - max(reference_date))
+  ]
 })
