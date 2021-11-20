f3a20e98ac9211c2bb1cb7f27b9e5fe9b13fea63
Sam Abbott <contact@samabbott.co.uk>
2021-11-20 23:39:04 +0000

    make by horizon plot function

--- R/plot.R
+++ R/plot.R
@@ -1,19 +1,14 @@
 plot_nowcasts_at_horizon <- function(summarised_nowcast, latest_obs,
-                                     target_horizon, ) {
+                                     max_delay = 28, ...) {
    epinowcast::enw_plot_nowcast_quantiles(
       summarised_nowcast[
-        reference_date < (max(nowcast_date) - 28)][,
-        holiday := NULL][,
-        horizon := as.numeric(as.Date(reference_date) - nowcast_date)][
-        location == locations][
-        horizon == -horizons][,
-        confirm := NA],
-      latest_obs = latest_7day_hospitalisations[
-        location == locations][
-        reference_date >= min(scored_nowcasts$reference_date)][
-        reference_date <= max(scored_nowcasts$reference_date)
+        reference_date < (max(nowcast_date) - ma_delay)][,
+        c("holiday", "confirm") := NULL][,
+        horizon := as.numeric(as.Date(reference_date) - nowcast_date)],
+      latest_obs = latest_obs[
+        reference_date >= min(summarised_nowcast$reference_date)][
+        reference_date <= max(summarised_nowcast$reference_date)
       ],
-      log = TRUE
-    ) +
-    facet_grid(vars(age_group), vars(model), scales = "free_y")
+      log = TRUE, ...
+    )
 }
