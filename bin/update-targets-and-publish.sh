10e2df2ae2c0f6ca298f6b07d70c8e1cb6798ce9
Sam Abbott <contact@samabbott.co.uk>
2021-11-20 23:01:06 +0000

    add rreport publishing

--- bin/update-targets-and-publish.sh
+++ bin/update-targets-and-publish.sh
@@ -6,4 +6,6 @@ bash bin/render-targets.sh > render.log 2>&1 &
 
 bash bin/make-parallel-targets.sh > targets.log 2>&1 &
 
-bash bin/update-remote.sh > git-remote.log 2>&1 &
+bash bin/update-git-remote.sh > git-remote.log 2>&1 &
+
+bash bin/update-targets-remote.sh > targets-remote.log 2>&1 &
