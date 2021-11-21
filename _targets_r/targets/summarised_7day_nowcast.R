07e9a4405ee710af87fa95de714a33f7f2dfb515
Sam Abbott <contact@samabbott.co.uk>
2021-11-21 11:47:26 +0000

    update adjustment function

--- _targets_r/targets/summarised_7day_nowcast.R
+++ _targets_r/targets/summarised_7day_nowcast.R
@@ -1,9 +1,10 @@
 tar_target(summarised_7day_nowcast, {
   combined_nowcasts |> 
-    adjust_posterior(
+    adjust_posteriors(
       target = "seven_day", 
       max_scale = 0.5, 
-      condition = "max_rhat > 1.1 | per_divergent_transitions > 0.2"
+      rhat_bound = 1.1,
+      per_dt_bound = 0.2
   ) |> 
     unnest_nowcasts(target = "seven_day") 
 })
