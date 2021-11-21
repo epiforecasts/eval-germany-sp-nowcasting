7abefac459adc717d8e4c24cfea63644ad36a76f
Sam Abbott <contact@samabbott.co.uk>
2021-11-21 23:35:40 +0000

    add basic plots to report

--- R/plot.R
+++ R/plot.R
@@ -1,14 +1,15 @@
-plot_nowcasts_at_horizon <- function(summarised_nowcast, latest_obs,
-                                     max_delay = 28, ...) {
-   epinowcast::enw_plot_nowcast_quantiles(
-      summarised_nowcast[
-        reference_date < (max(nowcast_date) - ma_delay)][,
-        c("holiday", "confirm") := NULL][,
-        horizon := as.numeric(as.Date(reference_date) - nowcast_date)],
+plot_nowcast <- function(summarised_nowcast, latest_obs,
+                                     max_delay = Inf, ...) {
+   summarised_nowcast <- summarised_nowcast[
+        reference_date >= (max(as.Date(nowcast_date)) - max_delay)][,
+        c("holiday") := NULL]
+
+   plot <- epinowcast::enw_plot_nowcast_quantiles(
+      summarised_nowcast,
       latest_obs = latest_obs[
         reference_date >= min(summarised_nowcast$reference_date)][
         reference_date <= max(summarised_nowcast$reference_date)
-      ],
-      log = TRUE, ...
-    )
+      ], ...
+  )
+  return(plot)
 }
--- R/utils.R
+++ R/utils.R
@@ -22,3 +22,46 @@ drop_string <- function(var, string) {
 select_var <- function(dt, var) {
   dt[[var]]
 }
+
+load_nowcasts <- function(path) {
+  nowcasts <- fs::dir_ls(
+    path,
+    glob = "*.csv"
+  ) |>
+    purrr::map(data.table::fread) |>
+    data.table::rbindlist()
+  nowcasts[, horizon := as.numeric(
+    as.Date(reference_date) - as.Date(nowcast_date)
+  )]
+  if (!is.null(nowcasts$age_group)) {
+    nowcasts[
+    ,
+    age_group := factor(
+      age_group,
+      levels = c("00+", "00-04", "05-14", "15-34", "35-59", "60-79", "80+")
+    )
+  ]
+  }
+  nowcasts[,
+   model := factor(
+    model,
+    levels = c("Reference: Fixed, Report: Fixed",
+               "Reference: Fixed, Report: Day of week",
+               "Reference: Age, Report: Day of week",
+               "Reference: Age and week, Report: Day of week",
+               "Reference: Age and week by age, Report: Day of week",
+               "Independent by age, Reference: Week, Report: Day of week")
+   )
+  ]
+  return(nowcasts)
+}
+
+fancy_datatable <- function(dt) {
+  dt <- janitor::clean_names(dt, case = "sentence")
+  DT::datatable(
+    dt,
+    extensions = c("Buttons", "Responsive"), options = list(
+      dom = "Bfrtip", buttons = c("csv")
+    )
+  )
+}
