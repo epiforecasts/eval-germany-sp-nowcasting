e74136c1b1335910065b6ad840af0584d11c46cc
Sam Abbott <contact@samabbott.co.uk>
2021-11-22 09:13:11 +0000

    2021-11-22 - nowcast update

--- bin/update-targets-and-publish.sh
+++ bin/update-targets-and-publish.sh
@@ -2,10 +2,10 @@
 
 git pull -X theirs
 
-bash bin/render-targets.sh > render.log 2>&1 &
+bash bin/render-targets.sh > render.log 2>&1
 
-bash bin/make-parallel-targets.sh > targets.log 2>&1 &
+bash bin/make-parallel-targets.sh > targets.log 2>&1
 
-bash bin/update-git-remote.sh > git-remote.log 2>&1 &
+bash bin/update-git-remote.sh > git-remote.log 2>&1
 
-bash bin/update-targets-remote.sh > targets-remote.log 2>&1 &
+bash bin/update-targets-remote.sh > targets-remote.log 2>&1
