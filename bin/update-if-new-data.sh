ae1c6eec24692b293e0c259470b8ca787e7bf8d4
Sam Abbott <contact@samabbott.co.uk>
2021-12-14 11:59:54 +0000

    add csv cache based check for pipeline running

---
+++ bin/update-if-new-data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+URL=https://raw.githubusercontent.com/KITmetricslab/hospitalization-nowcast-hub/main/data-truth/COVID-19/COVID-19_hospitalizations_preprocessed.csv
+wget -nc $URL -P data/
+
+CSV=data/COVID-19_hospitalizations_preprocessed.csv 
+
+if [ ! -f $CSV ] 
+  then
+  echo No cached data found - updating estimates
+  #bin/update-targets-and-publish.sh
+  rm $CSV
+  else
+  echo Cache found - pipeline running - no action taken
+fi
